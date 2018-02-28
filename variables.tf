884f4bc5b58265a196d6f649c0b8e66728e28ce3
WinstonKamau <crispus.kamau@andela.com>
2018-02-28 11:47:33 +0300

    feat(variables): add variables to the terraform script

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type="string"
+    default="us-east1"
+}
+
+variable "zone" {
+  type = "string"
+  default = "us-east1-b"
+}
+variable "project" {}
+
+variable "database-uri" {}
+
+variable "ip-address" {}
